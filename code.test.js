1d646ce95893c96413d62ddb667141565a7267b5
Ryan Miller <rmille70@uwyo.edu>
2024-04-28 19:38:32 -0600

    Changed code.test.js

--- code.test.js
+++ code.test.js
@@ -5,48 +5,43 @@ eval(fs.readFileSync('code.js')+'');
 
 let testCases = [
     {
-        graph1: [[1, 2], [0, 2, 3], [0, 1, 3], [1, 2]],
-        graph2: [[0, 2], [1, 2, 3], [0, 1, 3], [0, 1]],
+        graph1: [ [1 , 2], [0 , 3], [0 , 3], [1 , 2 , 4], [3], ],
+        graph2: [ [4], [4 , 2], [1 , 3], [2 , 4], [0 , 1 , 3], ],
         expectResult: true,
     },
   
     {
-        graph1: [[1, 2], [0, 2, 3], [0, 1, 3], [1, 2]],
-        graph2: [[0], [1], [2]], // Different vertex count
+        graph1: [ [1 , 2], [0 , 3], [0 , 3], [1 , 2 , 4], [3], ],
+        graph2: [ [1], [0, 2], [1, 3, 4], [2], [2, 5, 6], [4], [4], ],
         expectResult: false,
     },
   
     {
-        graph1: [[1, 2], [0, 2], [0, 1]],
-        graph2: [[0], [1], [1]], // Different degree sequences
+        graph1: [ [1], [0, 2], [1, 3, 4], [2], [2, 5, 6], [4], [4], ],
+        graph2: [ [3], [3], [4, 6], [0, 1, 4], [2, 3, 5], [4], [2], ],
         expectResult: true,
     },
   
     {
-        graph1: [[1], [2], [3]],
-        graph2: [[1], [0]], // Vertex 0 in graph2 has no corresponding neighbor
+        graph1: [ [1 , 2], [0 , 3], [0 , 3], [1 , 2 , 4], [3], ],
+        graph2: [ [1 , 2], [0 , 3], [0 , 3], [1 , 2 , 4], [3, 0], ],
         expectResult: false,
     },
   
-    {
-        graph1: [[1, 2], [0], [3]],
-        graph2: [[0, 2], [1], [3]],
-        expectResult: true,
-    }
 ];
 
 function runTests(testCase) {
-    let allPassed = true;  // Flag to track overall test pass/fail
+    let allPassed = true; 
     for (const thisCase of testCase) {
-        const { graph1, graph2, expectResult } = thisCase;
-        const actualResult = are_isomorphic(graph1, graph2);
+        let { graph1, graph2, expectResult } = thisCase;
+        let actualResult = are_isomorphic(graph1, graph2);
         if (actualResult !== expectResult) {
             console.error(`Test Case Failed:`);
             console.error(`  - Graph 1: ${JSON.stringify(graph1)}`);
             console.error(`  - Graph 2: ${JSON.stringify(graph2)}`);
             console.error(`  - Expected Result: ${expectResult}`);
             console.error(`  - Actual Result: ${actualResult}`);
-            allPassed = false; // Mark test failure
+            allPassed = false;
         }
     }
     return allPassed;
